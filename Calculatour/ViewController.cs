42591c6cf7c23740125ccc52c9aadfa9c5cf0970
numanmuttaqi <numan.muttaqi@tum.de>
2024-03-04 11:56:44 +0100

    Fresh from youtube tutorial for next update

---
+++ Calculatour/ViewController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace Calculatour
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+		int step = 0;
+		int num1;
+		int num2;
+
+        partial void PushButton(NSButton sender)
+        {
+			int num1 = Convert.ToInt16(TextInput.StringValue);
+			int num2 = Convert.ToInt16(TextInput2.StringValue);
+
+			if(selected == 0)
+			{
+				SetText(Convert.ToString(num1 + num2));
+			}
+            else if (selected == 1)
+            {
+                SetText(Convert.ToString(num1 - num2));
+            }
+            else if (selected == 2)
+            {
+                SetText(Convert.ToString(num1 * num2));
+            }
+            else if (selected == 3)
+            {
+                SetText(Convert.ToString(num1 / num2) + " remainder " + Convert.ToString(num1 % num2));
+            }
+            TextInput.StringValue = "";
+            TextInput2.StringValue = "";
+        }
+
+		private void SetText(string text)
+		{
+			InfoLabel.StringValue = text;
+		}
+
+		int selected = 0;
+
+        partial void AddAction(NSButton sender)
+        {
+			SetRadio(AddOutlet);
+			selected = 0;
+        }
+        partial void SubstractAction(NSButton sender)
+        {
+            SetRadio(SubstractOutlet);
+            selected = 1;
+        }
+        partial void MultiplyAction(NSButton sender)
+        {
+            SetRadio(MultiplyOutlet);
+			selected = 2;
+        }
+        partial void DivideAction(NSButton sender)
+        {
+            SetRadio(DivideOutlet);
+            selected = 3;
+        }
+
+        public void SetRadio(NSButton selected)
+		{
+			AddOutlet.State = NSCellStateValue.Off;
+            SubstractOutlet.State = NSCellStateValue.Off;
+            MultiplyOutlet.State = NSCellStateValue.Off;
+            DivideOutlet.State = NSCellStateValue.Off;
+
+            selected.State = NSCellStateValue.On;
+        }
+    }
+}
---
+++ Calculatour/ViewController.designer.cs
@@ -0,0 +1,92 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Calculatour
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSButton AddOutlet { get; set; }
+
+		[Outlet]
+		AppKit.NSButton DivideOutlet { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField InfoLabel { get; set; }
+
+		[Outlet]
+		AppKit.NSButton MultiplyOutlet { get; set; }
+
+		[Outlet]
+		AppKit.NSButton SubstractOutlet { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField TextInput { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField TextInput2 { get; set; }
+
+		[Action ("AddAction:")]
+		partial void AddAction (AppKit.NSButton sender);
+
+		[Action ("DivideAction:")]
+		partial void DivideAction (AppKit.NSButton sender);
+
+		[Action ("MultiplyAction:")]
+		partial void MultiplyAction (AppKit.NSButton sender);
+
+		[Action ("PushButton:")]
+		partial void PushButton (AppKit.NSButton sender);
+
+		[Action ("ResetButton:")]
+		partial void ResetButton (AppKit.NSButton sender);
+
+		[Action ("SubstractAction:")]
+		partial void SubstractAction (AppKit.NSButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (InfoLabel != null) {
+				InfoLabel.Dispose ();
+				InfoLabel = null;
+			}
+
+			if (AddOutlet != null) {
+				AddOutlet.Dispose ();
+				AddOutlet = null;
+			}
+
+			if (SubstractOutlet != null) {
+				SubstractOutlet.Dispose ();
+				SubstractOutlet = null;
+			}
+
+			if (MultiplyOutlet != null) {
+				MultiplyOutlet.Dispose ();
+				MultiplyOutlet = null;
+			}
+
+			if (DivideOutlet != null) {
+				DivideOutlet.Dispose ();
+				DivideOutlet = null;
+			}
+
+			if (TextInput != null) {
+				TextInput.Dispose ();
+				TextInput = null;
+			}
+
+			if (TextInput2 != null) {
+				TextInput2.Dispose ();
+				TextInput2 = null;
+			}
+		}
+	}
+}
